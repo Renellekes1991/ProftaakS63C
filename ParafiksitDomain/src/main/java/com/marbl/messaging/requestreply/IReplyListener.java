5c1d05c748a80813dc62651ec10767f45749f98f
rick963 <rick963@users.noreply.github.com>
2015-06-29 13:12:59 +0200

    aaaah

--- ParafiksitDomain/src/main/java/com/marbl/messaging/requestreply/IReplyListener.java
+++
@@ -1,9 +0,0 @@
-package com.marbl.messaging.requestreply;
-
-/**
- *
- * @author Maja Pesic
- */
-public interface IReplyListener<REQUEST,REPLY> {
-    public void onReply(REQUEST request,REPLY reply);
-}
--- ParafiksitDomain/src/main/java/com/marbl/messaging/requestreply/IRequestListener.java
+++
@@ -1,9 +0,0 @@
-package com.marbl.messaging.requestreply;
-
-/**
- *
- * @author Maja Pesic
- */
-public interface IRequestListener<REQUEST> {
-   public void receivedRequest(REQUEST request);
-}
